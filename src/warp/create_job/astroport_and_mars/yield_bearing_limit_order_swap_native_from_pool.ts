acb57fa1e4d873c90a692c39a7d3718bbad4b2f2
llllllluc <58892938+llllllluc@users.noreply.github.com>
2023-08-19 23:47:33 -0700

    yield bearing limit order working now

--- src/warp/create_job/astroport_and_mars/yield_bearing_limit_order_swap_native_from_pool.ts
+++ src/warp/create_job/astroport_and_mars/yield_bearing_limit_order_swap_native_from_pool.ts
@@ -1,11 +1,13 @@
 import Big from "big.js";
-import { MsgExecuteContract } from "@terra-money/feather.js";
+import { MsgExecuteContract, MsgSend } from "@terra-money/feather.js";
 import {
   createSignBroadcastCatch,
   getLCD,
   getMnemonicKey,
   getWallet,
+  getWarpFirstFreeSubAccountAddress,
   getWarpJobCreationFeePercentage,
+  queryWasmContractWithCatch,
   toBase64,
 } from "../../../util";
 import {
@@ -15,6 +17,7 @@ import {
   NTRN_USDC_PAIR_ADDRESS,
   USDC_DENOM,
   WARP_CONTROLLER_ADDRESS,
+  WARP_RESOLVER_ADDRESS,
 } from "../../../env";
 
 const mnemonicKey = getMnemonicKey();
@@ -43,10 +46,12 @@ const run = async () => {
   // maybe i don't need to specify spread in swap msg, as condition already ensure i get the price i want
   const maxSpread = "0.1";
 
-  const swapAmount = (100_000).toString();
+  // 0.01 USDC
+  const swapAmount = (10_000).toString();
 
   const offeredTokenDenom = usdcDenom;
 
+  // 0.01 NTRN
   const jobReward = (10_000).toString();
 
   // console.log("myAddress", myAddress);
@@ -54,36 +59,42 @@ const run = async () => {
   //   console.log(res);
   // })
 
+  const warpResolverAddress = WARP_RESOLVER_ADDRESS!;
+
+  const subAccountAddress = await getWarpFirstFreeSubAccountAddress(lcd, myAddress);
+
   const warpCreationFeePercentages = await getWarpJobCreationFeePercentage(lcd);
 
   const jobRewardAndCreationFee = Big(jobReward)
     .mul(Big(warpCreationFeePercentages).add(100).div(100))
     .toString();
 
-  /// init_msgs
+  const cosmosMsgEoaDepositToSubAccount = new MsgSend(myAddress, subAccountAddress, {
+    [offeredTokenDenom]: swapAmount,
+    [CHAIN_DENOM]: jobRewardAndCreationFee,
+  });
+
   const depositToMarsMsg = {
     deposit: {},
   };
 
-  const depositToMars = {
-    wasm: {
-      execute: {
-        contract_addr: marsRedBankAddress,
-        msg: toBase64(depositToMarsMsg),
-        funds: [{ denom: offeredTokenDenom, amount: swapAmount }],
-      },
+  const cosmosMsgSubAccountDepositToMars = new MsgExecuteContract(myAddress, subAccountAddress, {
+    generic: {
+      msgs: [
+        {
+          wasm: {
+            execute: {
+              contract_addr: marsRedBankAddress,
+              msg: toBase64(depositToMarsMsg),
+              funds: [{ denom: offeredTokenDenom, amount: swapAmount }],
+            },
+          },
+        },
+      ],
     },
-  };
+  });
 
-  /// vars
-  const jobVarNameAccountAddress = "account_address";
-  const jobVarAccountAddress = {
-    static: {
-      kind: "string",
-      name: jobVarNameAccountAddress,
-      value: "", // value will be set in the reply of create_account_and_job after account is created
-    },
-  };
+  /// =========== vars ===========
 
   const astroportSimulateNativeSwapMsg = {
     simulation: {
@@ -97,12 +108,10 @@ const run = async () => {
       },
     },
   };
-
-  const jobVarNamePrice = "ntrn-usdc-price";
+  const jobVarNamePrice = "ntrn_usdc_price";
   const jobVarPrice = {
     query: {
-      // kind: 'int', // uint, amount, decimal are all allowed
-      kind: "amount", // only int is not allowed since it expects result to be number, in fact result is string
+      kind: "amount",
       name: jobVarNamePrice,
       init_fn: {
         query: {
@@ -116,21 +125,20 @@ const run = async () => {
         selector: "$.return_amount",
       },
       reinitialize: false,
+      encode: false,
     },
   };
 
   const queryMarsBalanceMsg = {
     user_collateral: {
-      user: `$warp.variable.${jobVarNameAccountAddress}`, // can we nest variable like this?
+      user: subAccountAddress,
       denom: offeredTokenDenom,
     },
   };
-
-  const jobVarNameMarsBalance = "usdc-balance-in-mars";
+  const jobVarNameMarsBalance = "usdc_balance_in_mars";
   const jobVarMarsBalance = {
     query: {
-      // kind: 'int', // uint, amount, decimal are all allowed
-      kind: "amount", // only int is not allowed since it expects result to be number, in fact result is string
+      kind: "amount",
       name: jobVarNameMarsBalance,
       init_fn: {
         query: {
@@ -144,10 +152,46 @@ const run = async () => {
         selector: "$.amount",
       },
       reinitialize: false,
+      encode: false,
     },
   };
 
-  /// condition
+  const astroportNativeSwapMsg = {
+    swap: {
+      offer_asset: {
+        info: {
+          native_token: {
+            denom: offeredTokenDenom,
+          },
+        },
+        amount: `$warp.variable.${jobVarNameMarsBalance}`,
+        // amount: swapAmount,
+      },
+      /*
+      Belief Price + Max Spread
+      If belief_price is provided in combination with max_spread, 
+      the pool will check the difference between the return amount (using belief_price) and the real pool price.
+      The belief_price +/- the max_spread is the range of possible acceptable prices for this swap.
+      */
+      // belief_price: beliefPrice,
+      // max_spread: '0.005',
+      max_spread: maxSpread,
+      // to: '...', // default to sender, need to set explicitly cause default is sub account
+      to: myAddress,
+    },
+  };
+  const jobVarNameAstroportSwapMsg = "astroport_swap_msg";
+  const jobVarAstroportSwapMsg = {
+    static: {
+      kind: "string",
+      name: jobVarNameAstroportSwapMsg,
+      value: JSON.stringify(astroportNativeSwapMsg),
+      encode: true,
+    },
+  };
+
+  /// =========== condition ===========
+
   const condition = {
     expr: {
       decimal: {
@@ -162,11 +206,11 @@ const run = async () => {
     },
   };
 
-  /// msgs
+  /// =========== job msgs ===========
+
   const withdrawFromMarsMsg = {
     withdraw: {
       // unset will default to withdraw full amount
-      // amount: "10000",
       denom: offeredTokenDenom,
     },
   };
@@ -180,69 +224,104 @@ const run = async () => {
     },
   };
 
-  const astroportNativeSwapMsg = {
-    swap: {
-      offer_asset: {
-        info: {
-          native_token: {
-            denom: offeredTokenDenom,
-          },
-        },
-        amount: `$warp.variable.${jobVarNameMarsBalance}`,
-      },
-      /*
-      Belief Price + Max Spread
-      If belief_price is provided in combination with max_spread, 
-      the pool will check the difference between the return amount (using belief_price) and the real pool price.
-      The belief_price +/- the max_spread is the range of possible acceptable prices for this swap.
-      */
-      // belief_price: beliefPrice,
-      // max_spread: '0.005',
-      max_spread: maxSpread,
-      // to: '...', // default to sender
-      to: myAddress,
-    },
-  };
   const nativeSwap = {
     wasm: {
       execute: {
         contract_addr: astroportNtrnUsdcPairAddress,
-        msg: toBase64(astroportNativeSwapMsg),
+        msg: `$warp.variable.${jobVarNameAstroportSwapMsg}`,
         funds: [{ denom: offeredTokenDenom, amount: `$warp.variable.${jobVarNameMarsBalance}` }],
       },
     },
   };
 
   const executeMsg = {
-    create_account_and_job: {
-      name: "astroport_yield_bearing_limit_order_usdc_to_ntrn_from_pool",
-      description: "yield bearing limit order",
+    create_job: {
+      name: "astroport_yield_bearing_limit_order_swap_usdc_to_ntrn_from_pool",
+      description: "woooooooo yield bearing limit order",
       labels: ["astroport", "mars"],
+      // set account explicitly if we want to use sub account, otherwise it will use default account
+      account: subAccountAddress,
       recurring: false,
       requeue_on_evict: false,
       reward: jobReward,
+      vars: JSON.stringify([jobVarPrice, jobVarMarsBalance, jobVarAstroportSwapMsg]),
       condition: JSON.stringify(condition),
-      // TODO: also claim mars rewards if available
+      // TODO: claim mars rewards if available and send back to owner's EOA
       msgs: JSON.stringify([JSON.stringify(withdrawFromMars), JSON.stringify(nativeSwap)]),
-      // msgs: JSON.stringify([JSON.stringify(nativeSwap)]),
-      vars: JSON.stringify([jobVarAccountAddress, jobVarPrice, jobVarMarsBalance]),
-      initial_msgs: JSON.stringify([JSON.stringify(depositToMars)]),
-      should_update_var_account_address: true,
     },
   };
 
-  console.log("executeMsg", executeMsg);
-
-  const createJobAccountAndJob = new MsgExecuteContract(
+  const cosmosMsgCreateNewSubAccountCreateJob = new MsgExecuteContract(
     myAddress,
     warpControllerAddress,
-    executeMsg,
-    {
-      [CHAIN_DENOM]: Big(jobRewardAndCreationFee).add(Big(swapAmount)).toString(),
-    }
+    executeMsg
   );
 
-  createSignBroadcastCatch(wallet, [createJobAccountAndJob]);
+  // create a new sub account since we just used the first free sub account
+  // this is optional if user has more free accounts
+  const cosmosMsgCreateNewSubAccount = new MsgExecuteContract(myAddress, warpControllerAddress, {
+    create_account: {
+      is_sub_account: true,
+    },
+  });
+
+  createSignBroadcastCatch(wallet, [
+    cosmosMsgEoaDepositToSubAccount,
+    cosmosMsgSubAccountDepositToMars,
+    cosmosMsgCreateNewSubAccountCreateJob,
+    cosmosMsgCreateNewSubAccount,
+  ]);
+
+  /// =========== debug ===========
+
+  // queryWasmContractWithCatch(lcd, warpResolverAddress, {
+  //   query_validate_job_creation: {
+  //     vars: JSON.stringify([
+  //       jobVarPrice,
+  //       jobVarMarsBalance,
+  //       jobVarAstroportSwapMsg,
+  //       // jobVarAstroportSwapFund,
+  //     ]),
+  //     condition: JSON.stringify(condition),
+  //     msgs: JSON.stringify([JSON.stringify(withdrawFromMars), JSON.stringify(nativeSwap)]),
+  //     // msgs: JSON.stringify([JSON.stringify(nativeSwap)]),
+  //   },
+  // }).then((res) => console.log(res));
+
+  // const hydratedVars = await queryWasmContractWithCatch(lcd, warpResolverAddress, {
+  //   query_hydrate_vars: {
+  //     // vars: JSON.stringify([jobVarPrice, jobVarMarsBalance, jobVarAstroportSwapMsg, jobVarAstroportSwapFund]),
+  //     vars: JSON.stringify([jobVarPrice, jobVarMarsBalance, jobVarAstroportSwapMsg])
+  //   },
+  // });
+
+  // console.log(JSON.stringify(hydratedVars, null, 2));
+
+  // queryWasmContractWithCatch(lcd, warpResolverAddress, {
+  //   query_resolve_condition: {
+  //     condition: JSON.stringify(condition),
+  //     vars: hydratedVars,
+  //   },
+  // }).then((res) => {
+  //   console.log(res);
+  // });
+
+  // const hydratedMsgs = await queryWasmContractWithCatch(lcd, warpResolverAddress, {
+  //   query_hydrate_msgs: {
+  //     msgs: JSON.stringify([JSON.stringify(withdrawFromMars), JSON.stringify(nativeSwap)]),
+  //     vars: hydratedVars,
+  //   },
+  // });
+
+  // console.log(JSON.stringify(hydratedMsgs, null, 2));
+
+  // const msg = new MsgExecuteContract(myAddress, subAccountAddress, {
+  //   generic: {
+  //     msgs: hydratedMsgs,
+  //   },
+  // });
+
+  // createSignBroadcastCatch(wallet, [msg]);
 };
 
 run();
